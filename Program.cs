5d0242ab6a381eb7e55f2f9db6fdaf1a9db85af3
Gustav Polhem <gustavpolhem@gmail.com>
2024-03-26 21:31:31 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿
+using System.Configuration;
+
+namespace FlashcardProject
+{
+    public class Program
+    {
+        public string? connectionString = ConfigurationManager.AppSettings.Get("ConnectionString");
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+}
